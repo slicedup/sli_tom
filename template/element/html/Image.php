7a20152bc9b68a07ac7b675eddc7cde9ac5597d0
Paul Webster <paul@webprogression.co.nz>
2011-06-30 21:54:09 +1200

    Add html\Image object.

---
+++ template/element/html/Image.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Slicedup: a fancy tag line here
+ *
+ * @copyright	Copyright 2011, Paul Webster / Slicedup (http://slicedup.org)
+ * @license 	http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace sli_tom\template\element\html;
+
+class Image extends \sli_tom\template\element\Html {
+
+	protected $_method = 'image';
+
+	protected $_params = array(
+		'path' => null
+	);
+}
+
+?>
