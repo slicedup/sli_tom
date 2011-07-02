967f374b587440da81128a02f323e3846d25d9e3
Paul Webster <paul@webprogression.co.nz>
2011-07-02 22:07:45 +1200

    Form submit element

---
+++ template/element/form/Submit.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Slicedup: a fancy tag line here
+ *
+ * @copyright	Copyright 2011, Paul Webster / Slicedup (http://slicedup.org)
+ * @license 	http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace sli_tom\template\element\form;
+
+class Submit extends \sli_tom\template\element\Form {
+
+	protected $_method = 'submit';
+
+	protected $_params = array(
+		'title' => null
+	);
+}
+
+?>
